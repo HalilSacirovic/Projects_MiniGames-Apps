d569c4fa231b6181f7206ec49583c431a6b19cca
HalilSacirovic <halilsacirovic03@gmail.com>
2023-03-24 14:45:42 +0100

    modalWindow good thing to put in project(websites)

---
+++ Modal_Window/script.js
@@ -0,0 +1,28 @@
+const modal = document.querySelector(".modal");
+const showModal = document.querySelectorAll(".show-modal");
+const overlay = document.querySelector(".overlay");
+
+const closeModal = document.querySelector(".close-modal");
+
+console.log(showModal);
+
+for(let i = 0 ;i < showModal.length; i++){
+showModal[i].addEventListener("click",function(){
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+})
+
+
+}
+
+
+const closeOverlay = function(){
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+}
+
+closeModal.addEventListener("click",closeOverlay);
+
+
+overlay.addEventListener("click",closeOverlay);
+
